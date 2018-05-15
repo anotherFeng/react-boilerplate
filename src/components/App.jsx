942cf5bc3970937bab10b0e3e3eddc650e3436c8
Feng Desktop <regnagleppod@gmail.com>
2018-05-15 03:53:48 -0400

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+var App = () => (
+  <div>
+    Hello React
+  </div>
+);
+
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
+window.App = App;
